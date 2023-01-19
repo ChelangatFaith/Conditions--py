eccc5ea40d65bfbb6a9f2ee074e1937119a20fc2
SamanthaFaith <115935667+ChelangatFaith@users.noreply.github.com>
2023-01-19 14:42:55 +0300

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,13 @@
+#take the input from the user and display whether its odd or even
+input_number =input("enter the number")
+int_num = int(input_number)
+print(input_number)
+
+#if and else conditions are used to test a binary conditions 
+#Both true and false .
+#You can use other operators to test a multiple conditions.
+if int_num % 2== 0:
+    print("This is an even number")
+
+else:
+    print("This is an odd number")    
